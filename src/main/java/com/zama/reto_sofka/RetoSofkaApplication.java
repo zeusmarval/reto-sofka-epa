7240e59bcd8fb683da8f85d620077ae16acdc842
zeusmarval <zeusmarval@gmail.com>
2023-11-06 15:30:24 -0400

    build: initial spring boot

---
+++ src/main/java/com/zama/reto_sofka/RetoSofkaApplication.java
@@ -0,0 +1,13 @@
+package com.zama.reto_sofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoSofkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoSofkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zama/reto_sofka/RetoSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zama.reto_sofka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoSofkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
